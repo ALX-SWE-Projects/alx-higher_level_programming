b3e23b7bdf9a852d99f9ad3fa4fe5981c1efa5f2
ayyyub777 <ayoub.bigharassine@gmail.com>
2024-05-21 06:44:42 -0700

    0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+
+if (!filePath) {
+    console.error('Please provide a file path as the first argument');
+    process.exit(1);
+}
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(data);
+});
+
